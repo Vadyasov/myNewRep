f8868d999214ad6a2bb5441d54041ab0bb43ce39
Konstantin Vadyasov <vadyasov@MacBook-Air-Konstantin.local>
2014-04-04 12:49:54 +0300

    Initial Commit

---
+++ testApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Konstantin Vadyasov on 04.04.14.
+//  Copyright (c) 2014 Konstantin Vadyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Konstantin Vadyasov on 04.04.14.
+//  Copyright (c) 2014 Konstantin Vadyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
