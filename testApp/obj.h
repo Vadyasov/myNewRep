60bbaa629826bf4db2cc8e20bd2ad85e9aeaa846
Konstantin Vadyasov <vadyasov@MacBook-Air-Konstantin.local>
2014-04-04 13:42:00 +0300

    new branch

---
+++ testApp/obj.h
@@ -0,0 +1,13 @@
+//
+//  obj.h
+//  testApp
+//
+//  Created by Konstantin Vadyasov on 04.04.14.
+//  Copyright (c) 2014 Konstantin Vadyasov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface obj : NSObject
+
+@end
---
+++ testApp/obj.m
@@ -0,0 +1,13 @@
+//
+//  obj.m
+//  testApp
+//
+//  Created by Konstantin Vadyasov on 04.04.14.
+//  Copyright (c) 2014 Konstantin Vadyasov. All rights reserved.
+//
+
+#import "obj.h"
+
+@implementation obj
+
+@end
